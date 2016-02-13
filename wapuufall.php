8782d85c6eb7febc2dd2f9b247b8be9fcf1c90b2
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2016-02-13 16:29:00 +0900

    functions name

--- wapuufall.php
+++ wapuufall.php
@@ -9,31 +9,31 @@ Author URI:  http://ticklecode.com/
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Domain Path: /languages
-Text Domain: my-toolset
+Text Domain: wapuufall
 */
 
-function pluginprefix_install() {
+function wapuufall_install() {
   // Clear the permalinks after the post type has been registered
   flush_rewrite_rules();
 }
-register_activation_hook(__FILE__,'pluginprefix_install');
+register_activation_hook(__FILE__,'wapuufall_install');
 
-function pluginprefix_deactivation() {
+function wapuufall_deactivation() {
   // Our post type will be automatically removed, so no need to unregister it
   
   // Clear the permalinks to remove our post type's rules
   flush_rewrite_rules();
 }
-register_deactivation_hook(__FILE__,'pluginprefix_deactivation');
+register_deactivation_hook(__FILE__,'wapuufall_deactivation');
 
 /**
 * Enqueu script and css
 */
-function script_css_add(){
+function wapuufall_script_css_add(){
   wp_enqueue_script('snowfall-jquery',plugins_url('/js/snowfall.jquery.js',__FILE__),array('jquery'));
   wp_enqueue_script('wapuufall-jquery',plugins_url('/js/wapuufall.js',__FILE__),array('snowfall-jquery'));
   
   wp_localize_script('wapuufall-jquery','wapuufall_plugin_url',plugins_url('',__FILE__));
   
 }
-add_action('wp_enqueue_scripts','script_css_add');
+add_action('wp_enqueue_scripts','wapuufall_script_css_add');
